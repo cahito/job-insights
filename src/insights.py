f98920ab426921d26ce1dc6431fd2a0bc624dbd4
cahito <cahito@gmail.com>
2022-10-20 19:23:41 -0300

    Feat - Requisito três cumprido.

--- src/insights.py
+++ src/insights.py
@@ -3,7 +3,7 @@ from . import jobs
 
 def get_unique_job_types(path):
     all_jobs = jobs.read(path)
-    result = {job["job_type"] for job in all_jobs}
+    result = {job['job_type'] for job in all_jobs}
 
     return result
 
@@ -27,21 +27,10 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    all_jobs = jobs.read(path)
+    result = {job['industry'] for job in all_jobs if job['industry'] != ''}
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return result
 
 
 def filter_by_industry(jobs, industry):
