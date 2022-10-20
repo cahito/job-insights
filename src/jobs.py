ad27ca425ca3db0285d1e0fe192df91d3a7873f8
cahito <cahito@gmail.com>
2022-10-20 18:39:39 -0300

    Feat - Requisito um cumprido.

--- src/jobs.py
+++ src/jobs.py
@@ -1,17 +1,14 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path, encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=',', quotechar='"')
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+        result = []
+        for row in reader:
+            result.append(row)
+
+    return result
