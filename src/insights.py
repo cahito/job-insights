6b71cb3ace2503fa976a9db5df435150f5ebf4d0
cahito <cahito@gmail.com>
2022-10-21 15:55:01 -0300

    Feat - Requisito sete cumprido.

--- src/insights.py
+++ src/insights.py
@@ -22,21 +22,9 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    result = [job for job in jobs if job["industry"] == industry]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return result
 
 
 def get_max_salary(path):
