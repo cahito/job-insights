15da1c95f2c1b7fb3522bc4518c66a1cdf6a4da5
cahito <cahito@gmail.com>
2022-11-01 15:40:13 -0300

    Feat - Requisito onze cumprido. Acho que entendi melhor o patch.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,24 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+import pytest
+from unittest.mock import patch
 
 
-def test_brazilian_jobs():
-    pass
+@pytest.fixture
+def mocked_dict():
+    return [
+        [
+            {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"},
+            {"titulo": "Motorista", "salario": "3000", "tipo": "full time"},
+        ],
+        [
+            {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+            {"title": "Motorista", "salary": "3000", "type": "full time"},
+        ],
+    ]
+
+
+def test_brazilian_jobs(mocked_dict):
+    dict_to_translate, translated_dict = mocked_dict
+    with patch("src.jobs.read", return_value=dict_to_translate):
+        result = read_brazilian_file("src/anything.csv")
+        assert result == translated_dict
